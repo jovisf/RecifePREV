07389925dcff7b36b16eb902962d9b3419979372
Seu Nome <jovisf30@gmail.com>
2023-10-09 23:06:18 -0300

    enum tipo added

--- financeiro-backend/src/routes/operacoes.routes.ts
+++ financeiro-backend/src/routes/operacoes.routes.ts
@@ -1,10 +1,11 @@
-import { getAllOperacoes, getUniqueOperacao, deleteOperacaoByID, updateOperacaobyID } from "../controllers/operacao.controller";
+import { getAllOperacoes, getUniqueOperacao, deleteOperacaoByID, updateOperacaobyID, getConsulta } from "../controllers/operacao.controller";
 
 const operacoesRoutes = app => {
     app.get("/operacoes", getAllOperacoes)
-    app.get("/operacoes/:id", getUniqueOperacao)
+    app.get("/operacoes/:fundoId", getUniqueOperacao)
     app.delete("/operacoes/:id", deleteOperacaoByID)
     app.put("/operacoes/:id", updateOperacaobyID)
+    app.get("/consulta/:fundoId", getConsulta) //lembrar de fazer uma propria estrutura para consulta depois
 }
 
 
